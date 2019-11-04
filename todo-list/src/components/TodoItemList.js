89ddcc68deb60e09b12d73c2eb38f9a4abbb55fe
Sunghyun01 <37784262+Sunghyun01@users.noreply.github.com>
2019-11-04 10:35:50 +0900

    todoList 미완

---
+++ todo-list/src/components/TodoItemList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+class TodoItemList extends Component {
+  render() {
+    const { todos, onToggle, onRemove } = this.props;
+
+    return (
+      <div>
+        <TodoItem text="리액트 예제 해보기"/>
+        <TodoItem text="TO DO LIST 만들기"/>
+        <TodoItem text="리액트 공부"/>
+      </div>
+    );
+  }
+}
+
+export default TodoItemList;
